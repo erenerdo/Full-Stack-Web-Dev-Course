cb8b9f87a7d5ec3d34842ad1206daeb50bc40a70
Eren Erdogan <erenerdo@Erens-iMac.local>
2017-07-04 19:08:08 -0400

    Finished section 8, Introduction to JavaScript

---
+++ Web Developer Udemy/Session 8 Introduction to JavaScript/ageCalculator.js
@@ -0,0 +1,2 @@
+var ageInYears = prompt("How old are you?");
+alert("You have been alive roughgly " + ageInYears * 365.25 + " days")
---
+++ Web Developer Udemy/Session 8 Introduction to JavaScript/script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is your name");
+alert("Great to meet you " + name);
+console.log("Really nice to meet you, " + name);
