197c3da045ee9325e85ee409ab08d011f2e0a568
Eren Erdogan <ererdogan20@gmail.com>
2017-07-06 14:19:46 -0400

    Finished Section 13 on DOM Manipulation

---
+++ Web Developer Udemy/Section 13 DOM Manipulation/selectorExercise.js
@@ -0,0 +1,4 @@
+var p2 = document.getElementById("first");
+var p3 = document.getElementByClassName("special")[0];
+var p4 = document.querySelector("#first");
+var p1 = document.querySelectorAll("#first")[0];
